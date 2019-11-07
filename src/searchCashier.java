16995cf30e0bacd2c60873033096005be30b758c
Tharani Gnanasegaram <tharani.gnanasegaram@ifsworld.com>
2019-11-07 12:42:32 +0530

    search cashier comment

--- src/searchCashier.java
+++ src/searchCashier.java
@@ -11,6 +11,7 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
+//search cashier
 public class searchCashier extends JPanel {
 	
 	JTextField idField;
