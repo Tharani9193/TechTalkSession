c3c659ae6c3e3d8d002621d8fe24ad123c50f60e
Tharani Gnanasegaram <tharani.gnanasegaram@ifsworld.com>
2019-11-07 12:42:32 +0530

    pdf amend modifiedsuccess

--- src/Sale.java
+++ src/Sale.java
@@ -13,6 +13,7 @@ import javax.swing.JLabel;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
 
+//last commit
 public class Sale extends JPanel {
 	private JTable sale;
 	private JTextField date;
