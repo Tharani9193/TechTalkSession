d4e9139ef833fa50d4a8e000e15bd12832277ca4
Tharani Gnanasegaram <tharani.gnanasegaram@ifsworld.com>
2019-11-07 08:25:21 +0530

    commented

--- src/addCashier new.java
+++ src/addCashier new.java
@@ -13,7 +13,7 @@ import java.awt.Color;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-//comment added
+//comment added modified
 public class addCashier extends JPanel {
 	
 	JTextField userField;
